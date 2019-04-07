092372babab1ffbf04fc0eafef7659f747d1814f
Ravikumar <aravikumar7@gmail.com>
2019-04-07 20:51:13 +0100

    Dynamically getting browser

--- src/main/java/tranxit/BrowserFactory.java
+++ src/main/java/tranxit/BrowserFactory.java
@@ -13,9 +13,6 @@ import java.net.URL;
 public class BrowserFactory {
 
     public static WebDriver getBrowser(){
-    //Jenkins - http://localhost:8084/
-    //java -jar selenium-server-standalone-3.141.59.jar -role hub
-    //java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.0.3:4444/grid/register -browser browserName=chrome
 
         System.setProperty("webdriver.chrome.driver","G:/driver/chromedriver");
         WebDriverManager.chromedriver().setup();
@@ -24,8 +21,7 @@ public class BrowserFactory {
         final String ACCESS_KEY = "fba3dead-f784-4860-ae5a-5a9c54d44b84";
         final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.eu-central-1.saucelabs.com/wd/hub";
         WebDriver driver = null;
-        //String browserName = System.getProperty("BROWSER");
-        String browserName="chrome";
+        String browserName = System.getProperty("BROWSER");
         System.out.println("browsername"+browserName);
         DesiredCapabilities capabilities;
 
